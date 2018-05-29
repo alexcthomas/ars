82ff9d9bab8606af4c5c143a82b3c6eff8332fb6
Alex Thomas <alexthomas00@gmail.com>
2018-05-29 19:22:09 +0100

    Working boost python bindings & boost random

--- src/ars.cpp
+++ src/ars.cpp
@@ -1,17 +1,45 @@
-#define BOOST_PYTHON_STATIC_LIB
-//#define PY_MAJOR_VERSION 3
-//#define PY_MINOR_VERSION 6
+//#define BOOST_PYTHON_STATIC_LIB
+#define BOOST_PYTHON_DYNAMIC_LIB
 
+#include <iostream>
 
 #include <boost/python.hpp>
+#include <boost/python/numpy.hpp>
+
+#include <boost/random/taus88.hpp>
+#include <boost/random/mersenne_twister.hpp>
+#include <boost/random/uniform_real.hpp>
+#include <boost/random/variate_generator.hpp>
+#include <boost/generator_iterator.hpp>
+
+namespace py = boost::python;
+namespace np = boost::python::numpy;
+
+typedef boost::taus88 base_generator_type;
+base_generator_type generator(0);
+
+boost::uniform_real<> uni_dist(0, 1);
+boost::variate_generator<base_generator_type&, boost::uniform_real<> > uni(generator, uni_dist);
 
 
-char const* SayHello()
+np::ndarray GetUniform(const int n)
 {
-	return "Hello, from c++ dll!";
+	np::initialize();
+	
+	double * data = new double[n];
+
+	for (int i = 0; i < n; i++) {
+		data[i] = uni();
+	}
+	
+	py::tuple shape = py::make_tuple(n);
+	np::dtype dtype = np::dtype::get_builtin<double>();
+	py::tuple stride = py::make_tuple(sizeof(double));
+	np::ndarray a = np::from_data(data, dtype, shape, stride, py::object());
+	return a;
 }
+
 BOOST_PYTHON_MODULE(ars)
 {
-	using namespace boost::python;
-	def("SayHello", SayHello);
+	py::def("GetUniform", GetUniform);
 }
--- test.py
+++ test.py
@@ -5,5 +5,13 @@ Created on Tue May 29 12:27:57 2018
 @author: alex
 """
 
-import ars
-ars.SayHello()
+import ars, time
+
+t0 = time.time()
+ret = ars.GetUniform(200000000)
+print('time',time.time() - t0)
+
+print(ret.shape)
+print(ret.dtype)
+print(ret.min())
+print(ret.max())
